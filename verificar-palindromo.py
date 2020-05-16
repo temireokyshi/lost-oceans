7edf41e7a9182a8c0a1dd8af601bf460fb122ed0
temireokyshi <61682005+temireokyshi@users.noreply.github.com>
2020-05-16 00:33:37 -0300

    Some sinals remover
    Removendo algumas pontuações
    ! ? ' " - : ; \ / - , .
    se tiver mais algum tipo de caractere que atrapalha a verificação me avise por gentileza

--- verificar-palindromo.py
+++ verificar-palindromo.py
@@ -8,13 +8,17 @@ def remover_acentos(txt):
 
 # Minha criação daqui para baixo
 entrada = input("Digite a frase que deseja verificar se é palíndromo: ")
+entrada = remover_acentos(entrada)
 saida = ""
 for letra in entrada:
-    if letra == " ":
-        saida += letra.strip()
+    if letra == " " or letra == "." or letra == "," or letra == "!" or letra == "?" or letra == "'" or letra == ":" or \
+            letra == "\"" or letra == "-":
+        saida += letra.strip(",.!?': \"-")
     else:
         saida += letra.lower()
 if saida == saida[::-1]:
     print("\"{}\" é palindromo!")
 else:
     print("\"{}\" NÃO é palindromo!")
+    
+#se tiver mais algum tipo de caractere que atrapalha a verificação me avise
